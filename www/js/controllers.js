2897107430c2452200fa3df4059eeda43edcd159
Arnis <arnislupiks@gmail.com>
2015-03-31 01:27:41 +0100

    changed camera function
    different approach to camera, better

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,7 +25,11 @@ angular.module('starter.controllers', [])
   doAuth();
 })
 
-.controller('DashCtrl', function($scope,  $cordovaCamera, auth) {
+.config(function($compileProvider){
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
+.controller('DashCtrl', function($scope, Camera, auth) {
 
   $scope.auth = auth;
   $scope.postAuthorId = auth.profile.user_id;
@@ -36,25 +40,22 @@ angular.module('starter.controllers', [])
   $scope.likes = 0;
 
   $scope.myData = new Firebase('https://chatcathere.firebaseio.com/Posts');
-
-  $scope.takePicture = function() {
-       var options = {
-           quality : 75,
-           destinationType : Camera.DestinationType.DATA_URL,
-           sourceType : Camera.PictureSourceType.CAMERA,
-           allowEdit : true,
-           encodingType: Camera.EncodingType.JPEG,
-           targetWidth: 300,
-           targetHeight: 300,
-           popoverOptions: CameraPopoverOptions,
-           saveToPhotoAlbum: false
-       };
-       $cordovaCamera.getPicture(options).then(function(imageData) {
-           $scope.postImage = "data:image/jpeg;base64," + imageData;
-       }, function(err) {
-           // An error occured. Show a message to the user
-       });
-   };
+  //takes picture
+  $scope.getPhoto = function() {
+    console.log('Getting camera');
+    Camera.getPicture().then(function(imageURI) {
+      console.log(imageURI);
+      $scope.postImage = imageURI;
+    }, function(err) {
+      console.err(err);
+    }, {
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+  });
+}
+   // save all parameters to database
    $scope.savePost = function(){
      $scope.myData.push({postAuthorId:$scope.postAuthorId,
                          postAuthor:$scope.postAuthor,
@@ -84,6 +85,7 @@ angular.module('starter.controllers', [])
         });
     }
 })
+
 //one click directive
 .directive('clickOnce', function($timeout) {
     return {
@@ -101,6 +103,7 @@ angular.module('starter.controllers', [])
         }
     };
 })
+
 //safe factory apply mode for modules
 .factory('safeApply', [function($rootScope) {
     return function($scope, fn) {
@@ -118,67 +121,65 @@ angular.module('starter.controllers', [])
         }
     }
 }])
+
 .controller('ChatsCtrl', function($scope,$filter, safeApply,$ionicPopup, auth, $http) {
-  //set arraylist with details from database
-  $scope.Posts = {};
-  $scope.myData = new Firebase('https://chatcathere.firebaseio.com/Posts');
-  $scope.myData.on('value', function(snapshot){
-    $scope.Posts = snapshot.val();
-    safeApply($scope);
-  },function(err){
-    console.log("hey there, you have an error in : " +err);
-  });
-  $scope.myData.doClick = function(item, event){
-    alert("clicked: "+ item.postAuthor);
-  }
-  //add likes to post
-  $scope.count = function(post){
-    $scope.like = post.likes + 1;
-    var likeRef = $scope.myData.child(post.$key);
-      likeRef.update({
-        likes:$scope.like
-      });
+    //set arraylist with details from database
+    $scope.Posts = {};
+    $scope.myData = new Firebase('https://chatcathere.firebaseio.com/Posts');
+    $scope.myData.on('value', function(snapshot){
+        $scope.Posts = snapshot.val();
+        safeApply($scope);
+      },function(err){
+        console.log("hey there, you have an error in : " +err);
+    });
+    $scope.myData.doClick = function(item, event){
+      alert("clicked: "+ item.postAuthor);
     }
-    //dislike post
-    $scope.dislike = function(post){
-      $scope.dislike = post.likes - 1;
+    //add likes to post
+    $scope.count = function(post){
+      $scope.like = post.likes + 1;
       var likeRef = $scope.myData.child(post.$key);
         likeRef.update({
-          likes:$scope.dislike
+          likes:$scope.like
         });
-      }
-  //delete post function
-  $scope.delete = function(post){
-    if (post.postAuthorId == auth.profile.user_id){
-      var removePost = $scope.myData.child(post.$key);
-      removePost.remove();
-      $scope.showAlert1();
-    }else{
-      console.log("you are not authorise to do it!");
-      $scope.showAlert();
-    }
-  }
-  // An alert dialog
-  $scope.showAlert = function() {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Sorry, you can\'t delete it!',
-    });
-    alertPopup.then(function(res) {
-      console.log('Thank you your pations');
-    });
-  };
-  $scope.showAlert1 = function() {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Your post has been deleted successfully!',
-    });
-    alertPopup.then(function(res) {
-      console.log('Thank you for deleting post');
-    });
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+      };
+      //dislike post
+      $scope.dislike = function(post){
+        $scope.dislike = post.likes - 1;
+        var likeRef = $scope.myData.child(post.$key);
+          likeRef.update({
+            likes:$scope.dislike
+          });
+      };
+      //delete post function
+      $scope.delete = function(post){
+        if (post.postAuthorId == auth.profile.user_id){
+          var removePost = $scope.myData.child(post.$key);
+          removePost.remove();
+          $scope.showAlert1();
+        }else{
+          console.log("you are not authorise to do it!");
+          $scope.showAlert();
+        }
+      };
+      // An alert dialog when post not deleted
+      $scope.showAlert = function() {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Sorry, you can\'t delete it!',
+        });
+        alertPopup.then(function(res) {
+          console.log('Thank you your pations');
+        });
+      };
+      // an alert function when post is deleted
+      $scope.showAlert1 = function() {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Your post has been deleted successfully!',
+        });
+        alertPopup.then(function(res) {
+          console.log('Thank you for deleting post');
+        });
+      };
 })
 
 .controller('AccountCtrl', function($scope,store,$state, auth) {
